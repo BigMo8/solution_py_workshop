96e674b550f69f4cb4cc030c55d632811ea9c5ff
Monika Birgiel Marszal <monikabirgiel@gmail.com>
2023-07-03 17:57:59 +0200

    Halfway to success - i will continue tommorow ;)

---
+++ 12_solutions_bool.py
@@ -0,0 +1,79 @@
+#Napisz funkcję, która przyjmuje argumenty `element` i `lista` i jeżeli dany element znajduje się na liście, to zwraca jego pozycję
+#(użyj metody `index`), w przeciwnym wypadku zwraca `-1`.
+
+def check_list(element, list):
+    el_pos = list.index(element)
+    for element in list:
+        element is list
+        if True:
+            print("present on the list in position", el_pos)
+        else:
+            print("do not exist in the list")
+    
+element1 = 'bread'
+list1 = ['cocumber', 'tomatoe', 'onion', 'bread']
+
+check_list(element1, list1)
+
+#Napisz funkcję `iloraz`, która przyjmuje argumenty `dzielna` i `dzielnik`. Jeżeli dzielnik jest różny od zera, funkcja powinna zwrócić
+#wynik dzielenia.  W przeciwnym wypadku powinna wypisać komunikat o błędzie.
+
+def iloraz(dzielna, dzielnik):
+    if dzielnik != 0:
+        wynik = dzielna/dzielnik
+        print(wynik)
+    else:
+        print("Nie dziel przez zero")
+
+iloraz(10,0)
+
+#Napisz funkcję, która porównuje dwie liczby.  Jako argumenty powinna przyjmować liczby `a` i `b`.  Jeżeli `a` jest większe od `b`
+#powinna zwrócić 1, jeżeli liczby są równe `0`, a jeżeli `a` jest mniejsze od `b`, `-1`.  Dodatkowo, w zależności od wyniku porównania, funkcja
+#powinna wypisać jeden z komunikatów: `a < b`, `a == b` lub `a > b`.
+
+def compare(a,b):
+    float(a)
+    float(b)
+    if a > b:
+        print('a > b')
+        return 1
+    elif a + b == 0:
+        print('a == b')
+        return 0
+    elif a < b:
+        print('a < b')
+        return -1
+        
+compare(0,9)
+
+#Dla każdego z następujących typów odszukaj wartość, dla której funkcja `bool` zwróci `True` i taką dla której zwróci `False`: string,
+#krotka, integer, float.
+
+
+
+def checking_if(complexlist):
+    complexlist = []
+    for part in complexlist:
+        if part is int():
+            print(part, "is an intiger")
+        elif part is str():
+            print(part, "is a string")
+        elif part is tuple:
+            print(part, "is a tuple")
+        elif part is float():
+            print(part, "is a float")
+
+complexlist1 = ['ana', 123, ('krotka', "stokrotka", 'tokrotka'), 3.14]
+checking_if(complexlist1)
+    
+#Napisz funkcję, która jako argument przyjmie listę i zwróci `True` jeżeli wszystkie elementy na tej liście są prawdziwe, albo `False` jeżeli
+# przynajmniej jeden element nie jest prawdziwy.
+
+def checkinglist (list):
+    for element in list:
+        element is True
+        if True:
+            return bool
+
+
+check_list(complexlist1)
