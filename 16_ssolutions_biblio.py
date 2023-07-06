93eb604c3a48217ef1997d9ff01f7fb676b2b286
Monika Birgiel Marszal <monikabirgiel@gmail.com>
2023-07-06 20:58:04 +0200

    16_solutions_biblio complemented

--- 16_ssolutions_biblio.py
+++ 16_ssolutions_biblio.py
@@ -16,15 +16,3 @@ pole_kola(1)
 #dzielącą obie daty.  Zwróć uwagę, że różnica może być większa niż jedna
 #doba.  W takim wypadku liczbę dni należy zamienić na sekundy.
 
-#a=datetime.datetime()
-#b=datetime.datetime()
-
-def difference_bet_dates (a, b):
-    if b < a :
-        return None
-    else:
-        difference = b-a
-        print(difference.seconds)
-
-difference_bet_dates()
-#difference_bet_dates(2023, 2021)
