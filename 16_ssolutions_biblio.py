cd8967937ed82483ba52b48577a0f88a2b3ef393
Monika Birgiel Marszal <monikabirgiel@gmail.com>
2023-07-06 20:59:45 +0200

    16_ssolutions_biblio complemented

--- 16_ssolutions_biblio.py
+++ 16_ssolutions_biblio.py
@@ -1,5 +1,6 @@
 import math 
 import datetime
+import random
 
 #Napisz funkcję, która zwróci wartość pola powierzchni koła
 #o zadanym promieniu (według wzoru `PI * r^2`, gdzie `r` to promień).
@@ -16,3 +17,32 @@ pole_kola(1)
 #dzielącą obie daty.  Zwróć uwagę, że różnica może być większa niż jedna
 #doba.  W takim wypadku liczbę dni należy zamienić na sekundy.
 
+
+def time_differenvce (date_a, date_b):
+    if date_a > date_b:
+        return None
+    else: 
+        in_days = date_b - date_a
+        in_seconds = in_days.total_seconds()
+        print("Liczba sekund pomiędzy datami:", in_seconds)
+      
+
+date_random = datetime.datetime(2023,5,10,23,15,0)
+date_today = datetime.datetime.today()
+
+time_differenvce(date_random, date_today)
+
+#Napisz funkcję, która przyjmie jako argument dowolną sekwencję
+#i zwróci *krotkę* z trzema losowo wybranymi z niej elementami.
+
+def return_tuple(sequence):
+    random_tuple=()
+    element1 = random.choice(sequence)
+    element2 = random.choice(sequence)
+    element3 = random.choice(sequence)
+    random_tuple = (element1, element2, element3)
+    print(random_tuple)
+
+
+sequence1 = ('a', 'but', 'c', 100, 2, 3)
+return_tuple(sequence1)
